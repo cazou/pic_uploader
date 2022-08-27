ee7a1e5af5ef39d8fd18f5399d84379ac196dd9d
Detlev Casanova <detlev.casanova@gmail.com>
2022-08-27 17:32:31 -0400

    Use configurable video device

--- pic_uploader.sh
+++ pic_uploader.sh
@@ -4,12 +4,14 @@
 PEM=/home/detlev/.ssh/lightsail.pem
 SERVER=wotah.detlevcasanova.be
 DATE=$(date +%Y%m%d%H%M%S)
+VIDEODEV=${1:-/dev/video0}
 
 # Take a bunch of pics
-gst-launch-1.0 v4l2src device=/dev/video0 num_buffers=50 ! \
+gst-launch-1.0 v4l2src device=${VIDEODEV} num_buffers=10 ! \
     video/x-raw,format=YUY2,width=1280,height=720 ! \
     videoconvert ! \
     jpegenc ! \
     filesink "location=/tmp/${DATE}.jpeg" || true
 
 scp -i ${PEM} /tmp/${DATE}.jpeg admin@${SERVER}:pics/${DATE}.jpeg || true
+rm -fr /tmp/${DATE}.jpeg
